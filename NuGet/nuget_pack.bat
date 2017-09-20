340079170856d7cb4a9c53df6720c8194a98e029
Armando Aguirre <armanio123@outlook.com>
2017-09-19 22:37:49 -0700

    Update for version of new release

--- NuGet/nuget_pack.bat
+++ NuGet/nuget_pack.bat
@@ -1,3 +1,4 @@
 ::nuget pack ..\ReportViewerForMvc\ReportViewerForMvc.csproj -Properties Configuration=Release
+robocopy ..\ReportViewerForMvc\bin\Release lib\net45 ReportViewerForMvc.dll /is
+
 nuget pack ReportViewerForMvc.nuspec -Exclude *.bat
-pause
